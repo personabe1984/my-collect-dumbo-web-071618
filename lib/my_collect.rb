ae3249c5c53d57087348ded42e13b83b77e47b96
Hanbi Hans Choi <personabe1984@gmail.com>
2018-07-06 17:41:02 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(arr)
+  index = 0
+  arr_holder = []
+  while index < arr.length
+    arr_holder.push(yield(arr[index]))
+    index += 1
+  end
+  return arr_holder
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+temp = my_collect(array) do |name|
+  name.split(" ").first
+end
+puts temp
